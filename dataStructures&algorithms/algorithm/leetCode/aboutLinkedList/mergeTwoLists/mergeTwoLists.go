672918fd7b4f9394ddb125db0c68c77a000a1322
谭晓珊 <tanxiaoshan@weimiao.cn>
2022-06-01 17:17:30 +0800

    update

--- dataStructures&algorithms/algorithm/leetCode/aboutLinkedList/mergeTwoLists/mergeTwoLists.go
+++ dataStructures&algorithms/algorithm/leetCode/aboutLinkedList/mergeTwoLists/mergeTwoLists.go
@@ -30,7 +30,7 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 			cur.Next = list1
 			break
 		}
-		// 会出现一条链表遍历完，另外一条链表没遍历完的情况，需要将没遍历的链表添加到结果链表中
+		// 会出现一条链表遍历完，另外一条链表没遍历完的情况，需要将没遍历完的链表添加到结果链表中
 		if list1.Val < list2.Val {
 			cur.Next = list1
 			list1 = list1.Next
