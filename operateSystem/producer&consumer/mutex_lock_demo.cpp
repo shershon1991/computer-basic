9f5f672cd7f05fe0b4f1873bbcdc152b024baeaa
tanxiaoshan <18810877074@163.com>
2024-06-24 11:01:31 +0800

    update

--- operateSystem/producer&consumer/mutex_lock_demo.cpp
+++ operateSystem/producer&consumer/mutex_lock_demo.cpp
@@ -8,33 +8,36 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int num = 0;
 
-void *producer(void*){
+void *producer(void *)
+{
     int times = 10000000;
-    while(times --){
-        // pthread_mutex_lock(&mutex);
+    while (times--)
+    {
+        pthread_mutex_lock(&mutex);
         num += 1;
-        // pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);
     }
 }
 
-void *comsumer(void*){
+void *comsumer(void *)
+{
     int times = 10000000;
-    while(times --){
-        // pthread_mutex_lock(&mutex);
+    while (times--)
+    {
+        pthread_mutex_lock(&mutex);
         num -= 1;
-        // pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex);
     }
 }
 
-
-int main(){
-    printf("Start in main function.");
+int main()
+{
+    printf("Start in main function. \n");
     pthread_t thread1, thread2;
     pthread_create(&thread1, NULL, &producer, NULL);
     pthread_create(&thread2, NULL, &comsumer, NULL);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-    printf("Print in main function: num = %d\n", num);
+    printf("Print in main function: num = %d \n", num);
     return 0;
 }
-
